a30c3db5455fd76b3eb55e6cf657cd9931743421
George Souza Farias <georgesouzafarias@gmail.com>
2020-01-07 18:24:44 -0300

    create the class init and was inclued the first class

---
+++ manifests/init.pp
@@ -0,0 +1,12 @@
+# @summary  Main class, includes all other classes.
+#
+# Main class, includes all other classes.
+#
+# @param remove_telnet_client
+#   Enables the controle that will remove the telnet net client. Default value: true.
+#
+# @example
+#   include system_hardening
+class system_hardening ( Boolean $remove_telnet_client) {
+  contain system_hardening::remove_telnet_client
+}
