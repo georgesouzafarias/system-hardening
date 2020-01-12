793348b7550e3ab540fe100aaeeb45fe30bae245
George Souza Farias <georgesouzafarias@gmail.com>
2020-01-12 20:30:44 -0300

    created two new controls for rsh-client and rsh-server

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,12 +6,18 @@
 #   Enables the control that will remove the telnet client. Default value: true.
 ## @param remove_telnet_server
 #   Enables the control that will remove the telnet server. Default value: true.
+## @param remove_rsh_server
+#   Enables the control that will remove the rsh server. Default value: true.
+## @param remove_rsh_client
+#   Enables the control that will remove the rsh client. Default value: true.
 #
 # @example
 #   include system_hardening
 class system_hardening (
   Boolean $remove_telnet_client,
   Boolean $remove_telnet_server,
+  Boolean $remove_rsh_server,
+  Boolean $remove_rsh_client,
   ) {
   contain system_hardening::remote_access_controls
 }
